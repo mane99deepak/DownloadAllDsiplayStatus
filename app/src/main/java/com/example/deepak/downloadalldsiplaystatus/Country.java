d6bdd3dd5fd9d3e38ce6a59be48bf9cdc7fc2871
mane99deepak <manedeepak44@gmail.com>
2016-04-19 18:54:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/deepak/downloadalldsiplaystatus/Country.java
@@ -0,0 +1,25 @@
+package com.example.deepak.downloadalldsiplaystatus;
+
+/**
+ * Created by Deepak on 4/19/2016.
+ */
+public class Country {
+
+    String code = null;
+    String name = null;
+
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
